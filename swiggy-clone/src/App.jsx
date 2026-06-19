c4978c0a6877cc3204947fb11c9a29aecf93a7df
Arun <arundevarajan346@gmail.com>
2026-06-19 08:40:39 +0530

    routing

--- swiggy-clone/src/App.jsx
+++ swiggy-clone/src/App.jsx
@@ -2,13 +2,22 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import './index.css'
 import { useState } from "react"
+import { Route,Routes } from "react-router-dom";
+import RestaurantDetails from "./components/RestaurantDetails";
+
 function App() {
   const[searchText,setSearchText]=useState('')
 
   return (
     <>
       <Header searchText={searchText} setSearchText={setSearchText} />
-      <Body searchText={searchText}/>
+      <Routes>
+        <Route path="/" element={ <Body searchText={searchText}/>}/>
+
+        <Route path="/restaurant" element={<RestaurantDetails/>}/>
+        
+      </Routes>
+     
     </>
   )
 }
---
+++ swiggy-clone/src/components/RestaurantDetails.jsx
@@ -0,0 +1,11 @@
+const RestaurantDetails=()=>{
+    return(
+        <div>
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+
+
+export default RestaurantDetails
