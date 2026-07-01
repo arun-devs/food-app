3a24e2b6aaa207522054980b3d3261e83a877a81
Arun <arundevarajan346@gmail.com>
2026-07-01 20:24:13 +0530

    Completed add/remove cart functionality with quantity controls

--- swiggy-clone/src/components/MenuItem.jsx
+++ swiggy-clone/src/components/MenuItem.jsx
@@ -1,8 +1,8 @@
 const MenuItem=({menuitm,setCartCount,setCartItems,cartItems})=>{
     function addToCart(){
         setCartCount(prev=>prev+1)
-        const isCartContain=cartItems.find((itm)=>itm.id===menuitm.id)
-        if (isCartContain){
+        const availableiItem=cartItems.find((itm)=>itm.id===menuitm.id)
+        if (availableiItem){
             setCartItems(prev=>(prev.map((item)=>
                 item.id===menuitm.id?{...item,qty:item.qty+1}:
                 item
@@ -12,6 +12,23 @@ const MenuItem=({menuitm,setCartCount,setCartItems,cartItems})=>{
             setCartItems(prev=>[...prev,{...menuitm ,qty:1}])
         }
     }
+    function removeFromCart(){
+        const availableiItem=cartItems.find((itm)=>itm.id===menuitm.id)
+        if (availableiItem){
+            if (availableiItem.qty>1){
+                setCartItems(temp=>temp.map((itm)=>(itm.id===menuitm.id)?{...itm,qty:itm.qty-1}:itm))
+                setCartCount(prev =>{console.log("Previous Count:", prev);
+                 return prev - 1})
+            }
+            else{ 
+                  setCartItems(temp=>temp.filter((itm)=>itm.id!==menuitm.id))  
+                  setCartCount(prev => prev - 1)
+            }
+        }
+
+     
+    }
+    const cartItem=cartItems.find((itm)=>itm.id==menuitm.id)
     return (
                 <>
 
@@ -19,7 +36,18 @@ const MenuItem=({menuitm,setCartCount,setCartItems,cartItems})=>{
                     <h2>{menuitm.name}</h2>
                     <div className=" flex gap-2">
                     <h2 className="text-orange-700">₹{menuitm.price}</h2>
-                    <button className="text-gray-950 " onClick={addToCart}>Add to Cart</button>
+                     {cartItem?(
+                        <>
+                        <button className="text-gray-950 " onClick={removeFromCart}>-</button>
+                            <span>{cartItem.qty}</span>
+                      <button className="text-gray-950 " onClick={addToCart}>+</button>
+                        </>
+                      
+                     )
+                     
+                      :
+                    (<button className="text-gray-950 " onClick={addToCart}>Add to cart</button>)
+                     } 
                     </div>
                     
                 </div>
